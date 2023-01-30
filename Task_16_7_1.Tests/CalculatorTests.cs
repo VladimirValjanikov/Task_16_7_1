cb43f625eadfd9c034c9c92fb2cf6890da948a01
Vladimir <valjanikov-vladimir@yandex.ru>
2023-01-30 21:38:29 +0300

    Добавьте файлы проекта.

---
+++ Task_16_7_1.Tests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Task_16_7_1.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalTest()
+        {
+            var calculator = new Calculator();
+            
+            Assert.Equals(8, calculator.Additional(6, 2));
+        }
+
+        [Test]
+        public void SubtractionTest()
+        {
+            var calculator = new Calculator();
+            
+            Assert.Equals(4, calculator.Subtraction(6, 2));
+        }
+
+        [Test]
+        public void MiltiplicationTest()
+        {
+            var calculator = new Calculator();
+
+            Assert.Equals(12, calculator.Miltiplication(6, 2));
+        }
+
+        [Test]
+        public void DivisionTest()
+        {
+            var calculator = new Calculator();
+
+            Assert.Equals(3, calculator.Division(6, 2));
+        }
+    }
+}
